07e5b71f44bc38270a33a47269c0d9bd0004ae40
Yoannis Jamar <yoannis.j@gmail.com>
2017-04-06 17:48:14 +0200

    debug ‘@at-start’ and ‘@at-end’

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ function findStart(endRule, ruleName)
   // loop over previous rules
   while (prev)
   {
-    prev = prev.prev();
-
     // if previous rule is a '@at-start' rule
-    if (prev.type == 'atRule' && prev.name == 'at-start')
+    if (prev.type == 'atrule' && prev.name == 'at-start')
     {
       // and matches given '@at-end' rule
       if (prev.params.startsWith(ruleName)) {
         return prev;
       }
     }
+
+    prev = prev.prev();
   }
 }
 
@@ -45,8 +45,8 @@ module.exports = postcss.plugin('atwrap', function atwrap(opts) {
       // collect sibling rules between '@at-start' and '@at-end'
       parent.each(function(rule) {
 
-        var index = rule.index();
-        if (index > startRule.index() && index < endRule.index())  {
+        var index = rule.parent.index(rule);
+        if (index > startRule.parent.index(startRule) && index < endRule.parent.index(endRule))  {
           // collect rule
           wrapped.push(rule.clone());
           // remove it from parent
